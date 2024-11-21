c645b37b8616f06caf69705efcfaf604deafbd58
adrian-pasek-prv <adrian.pasek@payu.com>
2024-11-21 15:27:35 +0100

    Add list_users_in_group initial script

--- api_url_header.py
+++ api_url_header.py
@@ -1,17 +1,18 @@
 import configparser
 
 parser = configparser.ConfigParser()
-parser.read("conf")
+parser.read("config")
 
 DBT_ACCOUNT_ID = parser.get("dbt_credentials", "account_id")
 DBT_ACCESS_TOKEN = parser.get("dbt_credentials", "access_token")
 
-def return_api_url_header(api_version="v3", account_id=DBT_ACCOUNT_ID):
+def return_api_url_header(api_version="v2", account_id=DBT_ACCOUNT_ID):
     """Returns the base URL and headers for the DBT API."""
     return {
-        "base_url": f"https://cloud.getdbt.com/api/{api_version}/accounts/{account_id}/",
+        "base_url": f"https://emea.dbt.com/api/{api_version}/accounts/{account_id}/",
         "headers": {
             "Accept": "application/json",
-            "Authorization": f"Bearer {DBT_ACCESS_TOKEN}"
+            "Authorization": f"Bearer {DBT_ACCESS_TOKEN}",
+            "Content-Type": "application/json"
         }
     }
